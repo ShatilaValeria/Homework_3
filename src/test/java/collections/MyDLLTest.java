0efbd0381dc1b689ffcb392e2a4897199cb3524d
ValeriaShatila <123481670+ValeriaShatila@users.noreply.github.com>
2023-02-17 16:01:55 +0300

    Change MyDLLTest

--- src/test/java/collections/MyDLLTest.java
+++ src/test/java/collections/MyDLLTest.java
@@ -4,51 +4,50 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import utilities.ListADT;
-import utilities.MyArrayList;
 import utilities.MyDLL;
 
-import java.util.Arrays;
-
 public class MyDLLTest {
-    private   MyDLL<Integer>  myDLLInteger = new MyDLL<>();
-    MyDLL<String>  myDLLString;
-    private MyDLL<Double>  myDLLDouble = new MyDLL<>();
-    private MyDLL<Boolean>  myDLLBoolean = new MyDLL<>();
+
+    private MyDLL<String> myDLLString;
 
     @BeforeMethod
     public void initArray() {
         myDLLString = new MyDLL<>();
     }
 
-    @Test(description = "Metod isEmpty()")
-    public void testIsEmpty() {
+    @Test(description = "Checking the method isEmpty() for the true", groups = "MyDLLTest")
+    public void testIsEmptyTrue() {
         Assert.assertTrue(myDLLString.isEmpty());
     }
 
 
-    @Test(description = "Metod isEmpty()")
-    public void testIsNotEmpty() {
+    @Test(description = "Checking the method isEmpty() for the false", groups = "MyDLLTest")
+    public void testIsNotEmptyFalse() {
         myDLLString.add("1");
         myDLLString.add("2");
         myDLLString.add("3");
         Assert.assertFalse(myDLLString.isEmpty());
     }
 
-    @Test(description = "Metod itoArray(E[] toHold)")
-    public void testItoArrayEx() {
-        Assert.assertThrows(NullPointerException.class, () -> myDLLInteger.toArray(null));
-        Assert.assertThrows(NullPointerException.class, () -> myDLLDouble.toArray(null));
+    @Test(description = "Checking the method itoArray(E[] toHold) for the Exception",
+            groups = "MyDLLTest")
+    public void testItoArrayException() {
         Assert.assertThrows(NullPointerException.class, () -> myDLLString.toArray(null));
-        Assert.assertThrows(NullPointerException.class, () -> myDLLBoolean.toArray(null));
     }
 
-    @Test(description = "Metod itoArray(E[] toHold)")
+    @Test(description = "Checking the method itoArray(E[] toHold)", groups = "MyDLLTest")
     public void testItoArray() {
-        String[] ex = {"a","b"};
+        String[] ex = {"a", "b", "c"};
         Assert.assertEquals(myDLLString.toArray(ex), ex);
     }
 
-    @Test(description = "Metod contains(E toFind)")
+    @Test(description = "Checking the method contains(E toFind) for the Exception",
+            groups = "MyDLLTest")
+    public void testToFindException() {
+        Assert.assertThrows(NullPointerException.class, () -> myDLLString.contains(null));
+    }
+
+    @Test(description = "Checking the method contains(E toFind)", groups = "MyDLLTest")
     public void testToFind() {
         myDLLString.add("a");
         myDLLString.add("b");
@@ -57,22 +56,25 @@ public class MyDLLTest {
         Assert.assertFalse(myDLLString.contains("f"));
     }
 
-    @Test(description = "Metod contains(E toFind)")
-    public void testToFindEx() {
-        Assert.assertThrows(NullPointerException.class, () -> myDLLString.contains(null));
+    @Test(description = "Checking the method Add(E toAdd) for the Exception", groups = "MyDLLTest")
+    public void testAddException() {
+        Assert.assertThrows(NullPointerException.class, () -> myDLLString.add(null));
     }
 
-    @Test(description = "Add(E toAdd)")
+    @Test(description = "Checking the method Add(E toAdd))", groups = "MyDLLTest")
     public void testAdd() {
         Assert.assertTrue(myDLLString.add("3"));
     }
 
-    @Test(description = "Add(E toAdd)")
-    public void testAddEx() {
-        Assert.assertThrows(NullPointerException.class, () -> myDLLString.add(null));
+    @Test(description = "Checking the method  addAll(ListADT<? extends E> toAdd) for the Exception",
+            groups = "MyDLLTest")
+    public void testAddAllException() {
+        ListADT<String> toAdd = new MyDLL<>();
+        Assert.assertThrows(NullPointerException.class, () -> toAdd.add(null));
     }
 
-    @Test(description = "addAll")
+    @Test(description = "Checking the method  addAll(ListADT<? extends E> toAdd)",
+            groups = "MyDLLTest")
     public void testAddAll() {
         ListADT<String> toAdd = new MyDLL<>();
         toAdd.add("a");
@@ -81,25 +83,46 @@ public class MyDLLTest {
         Assert.assertEquals(myDLLString.toArray(), toAdd.toArray());
     }
 
-    @Test(description = "remove(E toRemove)")
+    @Test(description = "Checking the method remove(E toRemove) for the Exception",
+            groups = "MyDLLTest")
+    public void testRemoveException() {
+        Assert.assertThrows(NullPointerException.class, () -> myDLLString.remove(null));
+    }
+
+    @Test(description = "Checking the method remove(E toRemove)", groups = "MyDLLTest")
     public void testRemove() {
         myDLLString.add("a");
         myDLLString.add("b");
         myDLLString.add("c");
         myDLLString.remove(1);
         String[] expected = {"a", "c"};
-
         Assert.assertEquals(myDLLString.toArray(), expected);
     }
 
-    @Test(description = "add(int index, E toAdd)")
-    public void testAddS() {
+    @Test(description = "Checking the method add(int index, E toAdd) for the Exception", groups = "MyDLLTest")
+    public void testAddSecondException() {
         myDLLString.add("a");
         myDLLString.add("b");
-        Assert.assertTrue(myDLLString.add(myDLLString.size()-1 , "c"));
+        Assert.assertThrows(NullPointerException.class, () -> myDLLString.add(2, null));
+        Assert.assertThrows(IndexOutOfBoundsException.class, () -> myDLLString.add(100, "100"));
     }
 
-    @Test(description = "get(int index) ")
+    @Test(description = "Checking the method add(int index, E toAdd)", groups = "MyDLLTest")
+    public void testAddSecond() {
+        myDLLString.add("a");
+        myDLLString.add("b");
+        Assert.assertTrue(myDLLString.add(myDLLString.size() - 1, "c"));
+    }
+
+    @Test(description = "Checking the method get(int index) for the Exception", groups = "MyDLLTest")
+    public void testGetException() {
+        myDLLString.add("a");
+        myDLLString.add("b");
+        Assert.assertThrows(IndexOutOfBoundsException.class, () -> myDLLString.get(3));
+    }
+
+
+    @Test(description = "Checking the method get(int index)", groups = "MyDLLTest")
     public void testGet() {
         myDLLString.add("a");
         myDLLString.add("b");
@@ -108,14 +131,38 @@ public class MyDLLTest {
         Assert.assertNotEquals(myDLLString.get(2), "a");
     }
 
-    @Test(description = "set(int index, E toChange)")
+    @Test(description = "Checking the method remove(int index) for the Exception",
+            groups = "MyDLLTest")
+    public void testRemoveSecondException() {
+        Assert.assertThrows(IndexOutOfBoundsException.class, () -> myDLLString.remove(-7));
+    }
+
+    @Test(description = "Checking the method remove(int index)", groups = "MyDLLTest")
+    public void testRemoveSecond() {
+        myDLLString.add("a");
+        myDLLString.add("b");
+        myDLLString.add("c");
+        myDLLString.remove(1);
+        String[] expected = {"a", "c"};
+        Assert.assertEquals(myDLLString.toArray(), expected);
+    }
+
+    @Test(description = "Checking the method set(int index, E toChange) for the Exception",
+            groups = "MyDLLTest")
+    public void testSetException() {
+        Assert.assertThrows(IndexOutOfBoundsException.class, () -> myDLLString.set(-7, "a"));
+        myDLLString.add("a");
+        myDLLString.add("b");
+        Assert.assertThrows(NullPointerException.class, () -> myDLLString.set(2, null));
+    }
+
+    @Test(description = "Checking the method set(int index, E toChange)", groups = "MyDLLTest")
     public void testSet() {
         myDLLString.add("a");
         myDLLString.add("b");
         myDLLString.add("c");
-        myDLLString.set(2,"f" );
-        String[] expected = {"a","b", "f"};
+        myDLLString.set(2, "f");
+        String[] expected = {"a", "b", "f"};
         Assert.assertEquals(myDLLString.toArray(), expected);
     }
-
 }
